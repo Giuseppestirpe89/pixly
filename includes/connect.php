eb4c0ad5357b70f070e81fe7bd10965d50c0b061
Don Whelan <donwhelan1612@gmail.com>
2016-10-19 12:02:37 +0000

    escape_data() added and tested

--- includes/connect.php
+++ includes/connect.php
@@ -1,5 +1,5 @@
 <?php
-//ref: http://www.newthinktank.com/2011/01/php-security/
+    //ref: http://www.newthinktank.com/2011/01/php-security/
 
     define("HOST", "dublinscoffee.ie");
     define("USER", "dubli653_dib");
@@ -23,5 +23,28 @@
     // while ($row = mysql_fetch_assoc($result)) {
     // echo $row['working'];
     // }
+    
+    
+    /*
+     *  Escape data function to strip text that is being sent to the DB of harmful tags and characters 
+     *  mysql_real_escape_string() is a more secure method of scrubbing data so we check if it is available, as it realize on a connection to the DB
+     *  If available we trim() to remove whitespace, then put pass through the mysql_real_escape_string() to address the threat of SQL injection.
+     *  The data is then run through strip_tags() to remove HTML tags like "<script>" to address XSS attacks.
+     *
+     *  If mysql_real_escape_string() in unavilable we do the same steps but using mysql_escape_string().
+     */
+    function escape_data($dataFromForms) {
+        if (function_exists('mysql_real_escape_string')) {
+            global $connection;
+            $dataFromForms = mysql_real_escape_string (trim($dataFromForms), $connection);
+            $dataFromForms = strip_tags($dataFromForms);
+            echo ("mysql_real_escape_string was available");
+        } else {
+            $dataFromForms = mysql_escape_string (trim($dataFromForms));
+            $dataFromForms = strip_tags($dataFromForms);
+            echo ("mysql_real_escape_string was not available");
+        }
+        return $dataFromForms;
+    }
 
 ?>
--- includes/testQuery.php
+++ includes/testQuery.php
@@ -2,11 +2,21 @@
 
     include("connect.php");
 
-    $query = "SELECT * FROM test";
+    $query = "Select * from test";
     $result = mysql_query($query);
     
     while ($row = mysql_fetch_assoc($result)) {
     echo $row['working'];
     }
+    
+    echo ("<br/><br/><br/>");
+    
+    $sample = "<br/><br/><br/>";
+    
+    $sample = escape_data($sample);
+    echo ("<br/><br/><br/>");
+    echo ("text");
+    echo $sample;
+    echo ("text");
     
 ?>
