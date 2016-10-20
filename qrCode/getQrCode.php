73d3618cbe8363ff23710dcdb93b12727b2d5c9a
Giuseppe Stirpe <giuseppestirpe89@gmail.com>
2016-10-20 20:24:33 +0000

    chart.js

---
+++ qrCode/getQrCode.php
@@ -0,0 +1,20 @@
+<?php
+
+header('Content-Type:image/png');
+
+require_once 'src/QrCode.php';
+
+use Endroid\QrCode\QrCode;
+
+//Queries the Endroid library and drills down to QRCode class to generate QRCode image
+$qr =  new Endroid\QrCode\QrCode();
+
+
+//
+$qr->setText ($_SERVER['REQUEST_URI']);
+$qr->setSize(200);
+$qr->setPadding(10);
+
+$qr->render();
+
+?>
