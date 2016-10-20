a1030d1683112d95c9b830b1758f37036ecea7c0
patrickking25 <kinpat7@aol.com>
2016-10-20 19:30:08 +0000

    QRCode Files

---
+++ urlPull/getURL.php
@@ -0,0 +1,20 @@
+<?php
+
+
+function getCurrentURL()
+{
+    $currentURL = (@$_SERVER["HTTPS"] == "on") ? "https://" : "http://";
+    $currentURL .= $_SERVER["SERVER_NAME"];
+ 
+    if($_SERVER["SERVER_PORT"] != "80" && $_SERVER["SERVER_PORT"] != "443")
+    {
+        $currentURL .= ":".$_SERVER["SERVER_PORT"];
+    } 
+ 
+        $currentURL .= $_SERVER["REQUEST_URI"];
+    return $currentURL;
+}
+
+echo getCurrentURL();
+
+?>
