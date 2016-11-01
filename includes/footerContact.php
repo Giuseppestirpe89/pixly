10ca07f0c639db1f5f3e5324baa030ddada5ba92
Giuseppe Stirpe <giuseppestirpe89@gmail.com>
2016-11-01 20:05:57 +0000

    footer contact

---
+++ includes/footerContact.php
@@ -0,0 +1,49 @@
+<div class="footer">
+            <div class="row">
+                <div class="btn-group btn-group-justified">
+                    <div class="btn-group">
+                        <button type="button" class="btn btn-nav">
+                     <a href="http://www.twitter.com" target="_blank"><span class="fa fa-twitter fa-5x"></span></a>
+                     <p>Twitter</p>
+                  </button>
+                    </div>
+                    <div class="btn-group">
+                        <button type="button" class="btn btn-nav">
+                    <a href="http://www.facebook.com" target="_blank"> <span class="fa fa-facebook fa-5x"></span></a>
+                     <p>Facebook</p>
+                  </button>
+                    </div>
+                    <div class="btn-group">
+                        <button type="button" class="btn btn-nav">
+                     <a href="http://www.youtube.com" target="_blank"><span class="fa fa-youtube fa-5x"></span></a>
+                     <p>Youtube</p>
+                  </button>
+                    </div>
+                    <div class="btn-group">
+                        <button type="button" class="btn btn-nav">
+                     <a href="http://www.google.com" target="_blank"><span class="fa fa-google-plus fa-5x"></span></a>
+                     <p>Google+</p>
+                  </button>
+                    </div>
+                    <div class="btn-group">
+                        <button type="button" class="btn btn-nav">
+                     <a href="http://www.instagram.com" target="_blank"><span class="fa fa-instagram fa-5x"></span></a>
+                     <p>Instagram</p>
+                  </button>
+                    </div>
+                    <div class="btn-group">
+                        <button type="button" class="btn btn-nav">
+                     <a href="http://www.pinterest.com" target="_blank"><span class="fa fa-pinterest fa-5x"></span></a>
+                     <p>Pinterest</p>
+                  </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- jQuery -->
+        <script src="../js/jquery.js"></script>
+        <!-- Bootstrap Core JavaScript -->
+        <script src="../js/bootstrap.min.js"></script>
+        <!-- Scrolling Nav JavaScript -->
+        <script src="../js/jquery.easing.min.js"></script>
+        <script src="../js/scrolling-nav.js"></script>
