c3d2adfcf9e3699ec66595a8d2c36898dfab094c
patrickking25 <kinpat7@aol.com>
2016-12-08 20:43:47 +0000

    Qr library comments

--- qrCode/getQrCode.php
+++ qrCode/getQrCode.php
@@ -2,19 +2,20 @@
 
 header('Content-Type:image/png');
 
+//Inherits the QRCode.php files and contents for reference
 require_once 'src/QrCode.php';
 
-use Endroid\QrCode\QrCode;
-
 //Queries the Endroid library and drills down to QRCode class to generate QRCode image
+use Endroid\QrCode\QrCode;
 $qr =  new Endroid\QrCode\QrCode();
 
 
-//
-$qr->setText ("https://fuckgithub-donwhelan.c9users.io/events.php?testtoken");
+//Sets the static text to form the QR Code, the size of the outputed image
+$qr->setText ("https://google.com?testtoken");
 $qr->setSize(200);
 $qr->setPadding(10);
 
+//Renders the QR Code as a png image rather than code
 $qr->render();
 
 ?>
